8fca1c882f1597133889482b7e02a99abf349468
Chanchal Kumar Mishra <chanchalmishra1947@gmail.com>
2022-07-05 10:23:47 +0530

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :todo_lists do
+    resources :todo_items do
+      member do
+        patch :complete
+      end
+    end
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "todo_lists#index"
+end
